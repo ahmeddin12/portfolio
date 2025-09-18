4004bf3a0df3569bb9e83f56cca5c16009ff0f5b
ahmeddin12 <amudi621@gmail.com>
2025-09-18 12:13:13 +0300

    added "@" as alias to refer './src'

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
 import path from "path";
-import { fileURLToPath } from "url";
 
-// ESM-safe __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
