c1b2327bf51a9601f5dbee16910d9cd5ee1d90ca
ahmeddin12 <amudi621@gmail.com>
2025-09-19 15:28:13 +0300

    added navbar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 export const Home = () => {
   return (
     <div>
       <ThemeToggle />
       <StarBackground />
+      <Navbar />
     </div>
   );
 };
