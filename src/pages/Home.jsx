8420502ae9fe560e248b5814f22a24992ac0e6f7
ahmeddin12 <amudi621@gmail.com>
2025-09-21 21:42:09 +0300

    added Hero and AboutMe component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 export const Home = () => {
   return (
     <div>
       <ThemeToggle />
       <StarBackground />
       <Navbar />
+
+      <main>
+        <HeroSection />
+        <AboutMe />
+      </main>
     </div>
   );
 };
