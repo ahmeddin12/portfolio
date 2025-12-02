14ea9d27de56e248f67d5c8900f31eb942993be5
ahmeddin12 <amudi621@gmail.com>
2025-12-02 23:23:29 +0300

    update CV link in AboutSection to new cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,14 +33,14 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Ahmed's_CV.pdf"
+                href="/Ahmed_Mahmud_CV2.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
               </a>
               <a
-                href="/Ahmed's_CV.pdf"
+                href="/Ahmed_Mahmud_CV2.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
